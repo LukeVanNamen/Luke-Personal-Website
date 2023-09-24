412fff8bf8a552279f5dbd8a89fa44035446131c
Luke Van Namen <vannamenluke1@gmail.com>
2023-09-23 18:40:12 -0700

    Adding Python Tests

---
+++ test-site/infrastructure/python_tests.py
@@ -0,0 +1,34 @@
+import unittest
+import requests
+import pytest
+
+class TestFrontEndSite(unittest.TestCase):
+    def test_web_app_running(self):
+        try:
+             r = requests.get("https://www.lukevannamen.com/#home")
+        except:
+            self.fail("Could not open web app. Not running, or crashed. Test Failed")
+
+    def test_text(self):
+        r = requests.get("https://www.lukevannamen.com/#home")
+        page_src = r.text
+ 
+        if page_src.find("Greenside") < 0:
+            self.fail("Can't find brand")
+        if page_src.find("Luke") < 0:
+            self.fail("Can't find name")
+        if page_src.find("Views") < 0:
+            self.fail("Can't find views")
+    
+    def test_projects(self):
+        r = requests.get("https://www.lukevannamen.com/#projects")
+        page_src = r.text
+
+        if page_src.find("Projects") < 0:
+            self.fail("Can't find Projects")
+
+    
+        
+
+if __name__ == '__main__':
+    unittest.main()
